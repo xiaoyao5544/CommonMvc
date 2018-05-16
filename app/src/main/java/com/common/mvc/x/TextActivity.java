9dc5bf29463207b86acc3e1c097a19955f6bec08
xiaoyao5544 <13581660783@163.com>
2018-05-16 23:45:16 +0800

    Initial commit

---
+++ app/src/main/java/com/common/mvc/x/TextActivity.java
@@ -0,0 +1,14 @@
+package com.common.mvc.x;
+
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class TextActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_text);
+    }
+}
